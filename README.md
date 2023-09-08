b702da328f9764e66091c01fc4b630b6a695daa0
allen <74865251a@gmail.com>
2023-09-08 15:54:35 +0800

    add backend

---
+++ README.md
@@ -0,0 +1,8 @@
+# ZKore backend
+A ZKML application to prove your off chain score on chain.
+
+## Run
+1. Run Backend
+```shell
+uvicorn server:app --reload
+```
---
+++ server.py
@@ -0,0 +1,38 @@
+import os
+from fastapi import FastAPI, Request, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from utils import get_twitter_score
+
+from pydantic import BaseModel
+
+
+import uvicorn
+
+import pandas as pd 
+
+app = FastAPI()
+
+# middleware
+app.add_middleware(
+    CORSMiddleware, 
+    allow_credentials=True, 
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
+
+class single_respond(BaseModel):
+    user_name : str
+
+@app.post("/single_respond/")
+async def single_respond(item: single_respond):
+    respond = get_twitter_score(item.user_name)
+    print(respond)
+    return {"code":0, "data":respond}
+
+    
+
+if __name__ == "__main__":
+    if not os.path.exists('./documents'):
+        os.makedirs('./documents')
+    uvicorn.run(app, host="0.0.0.0", port=8000)
